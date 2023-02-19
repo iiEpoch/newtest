04c9352279e573b65880860efa5ba98509b9839b
JingYu <92147862+iiEpoch@users.noreply.github.com>
2023-02-19 14:56:22 +0800

    Add files via upload

---
+++ prepare128B.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+path="/mnt/nvme5/128"
+
+path_old="/mnt/nvme5/4096"
+
+rm -rf ${path_old}
+
+value_size=128
+
+mkdir ${path}/${value_size}B
+
+for dataset in {200,400}
+do	
+	echo "创建${dataset}G-${value_size}B数据集"
+	./writedata.sh ${path} ${dataset} ${value_size}
+	sleep 10
+done
